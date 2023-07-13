278186f0de7b079d0194355d8029dd50152c92dc
Tomas Villarreal <tomas.villarreal@unc.edu.ar>
2023-07-13 20:25:49 -0300

    [CNC-12] Agregadas rutas protegidas, authContext y componente de proyectos sin estilos

--- src/App.js
+++ src/App.js
@@ -2,12 +2,14 @@ import './css/style.css'
 import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 // Import components
+import RequireAuth from './js/components/RequireAuth/RequireAuth'
 
 import Login from './js/pages/Login'
 import Signup from './js/pages/Signup'
 import Home from './js/pages/Home'
 import Unauthorized from './js/pages/Unauthorized'
 import NotFound from './js/pages/NotFound'
+import Projects from './js/pages/Projects'
 
 
 function App() {
@@ -20,6 +22,9 @@ function App() {
           <Route path='/signup' element={<Signup/>}/>
           <Route path='/unauthorized' element={<Unauthorized/>}/>
 
+          <Route element={<RequireAuth />}>
+            <Route path='/projects' element={<Projects/>}/>
+          </Route>
 
           <Route path='*' element={<NotFound/>}/>
 
--- src/js/pages/Login.jsx
+++ src/js/pages/Login.jsx
@@ -1,6 +1,11 @@
 import Button  from '../components/Button/Button'
 import { useState } from 'react'
 import { useFormValidator } from '../hooks/useFormValidator'
+import useAuth from '../hooks/useAuth'
+
+import axios from '../../api/axios'
+
+const LOGIN_URL = '/api/v1/auth/login'
 
 export default function Login() {
   const [formValues, setFormValues] = useState({
@@ -9,6 +14,8 @@ export default function Login() {
   })
 
   const {errors, validateForm, onBlurField} = useFormValidator(formValues)
+
+  const { setAuth } = useAuth()
 
   const handleChange = (e) => {
     const {name, value} = e.target
@@ -22,12 +29,43 @@ export default function Login() {
     }
   }
 
-  const handleSubmit = (e) => {
+  const handleSubmit = async (e) => {
     e.preventDefault()
     const { isValid } = validateForm({form: formValues, errors, forceTouchErrors: true})
-
     if(!isValid) return
 
+    try {
+      const { email, password } = formValues
+      const response = await axios.post(LOGIN_URL, 
+        JSON.stringify({email, password}),
+          {
+            headers: {'Content-Type': 'application/json'},
+            withCredentials: false
+
+          }
+      )
+      console.log(JSON.stringify(response?.data))
+
+      const accessToken = response?.data?.accessToken
+      const roles = response?.data?.roles
+      setAuth({email, password, roles, accessToken})
+
+      setFormValues({
+        email: '',
+        password: ''
+      })
+    } catch (err) {
+      if(!err?.response){
+        console.log('El servidor no respondio')
+      } else if(err.response?.status === 403) {
+        console.log('Datos incorrectos intente nuevamente')
+      } else if(err.response?.status === 401) {
+        console.log('No estas autorizado para realizar esta operacion')
+      } else {
+        console.log('Fallo el logueo')
+      }
+
+    }
     console.log('Se mando XD')
   }
 
@@ -51,6 +89,7 @@ export default function Login() {
                 onChange={handleChange}
                 onBlur={onBlurField}
                 value={formValues.email}
+                required
             />
         </label>
         {errors.email.dirty && errors.email.error ? (<small className='login__error'>{errors.email.message}</small>) : null} 
@@ -63,6 +102,7 @@ export default function Login() {
                 onChange={handleChange}
                 onBlur={onBlurField}
                 value={formValues.password}
+                required
             />
         </label>
         {errors.password.dirty && errors.password.error && <p className='login__error'>{errors.password.message}</p>}
