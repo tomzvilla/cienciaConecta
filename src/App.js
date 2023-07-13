824d513127bc7fa2b8e5ea3bf2ce6f2f9d705418
Tomas Villarreal <tomas.villarreal@unc.edu.ar>
2023-07-13 17:30:49 -0300

    [CNC-12] Agregado componente home sin estilos

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import { BrowserRouter, Route, Routes } from 'react-router-dom'
 
 import Login from './js/pages/Login'
 import Signup from './js/pages/Signup'
+import Home from './js/pages/Home'
 
 
 function App() {
@@ -12,6 +13,7 @@ function App() {
     <div className="App">
       <BrowserRouter>
         <Routes>
+          <Route path='/*' element={<Home/>}/>
           <Route path='/login' element={<Login/>}/>
           <Route path='/signup' element={<Signup/>}/>
         </Routes>
---
+++ src/js/pages/Home.jsx
@@ -0,0 +1,9 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div className='home'>
+        <h2>Este es el HOME</h2>
+    </div>
+  )
+}
