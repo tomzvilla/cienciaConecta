34da00c95271a45f75594f398afffd88927f4ffc
Tomas Villarreal <tomas.villarreal@unc.edu.ar>
2023-07-15 20:58:08 -0300

    [CNC-12] Cambio en el RequireAuth de auth.email por auth.cuil

--- src/js/components/RequireAuth/RequireAuth.jsx
+++ src/js/components/RequireAuth/RequireAuth.jsx
@@ -8,7 +8,7 @@ const RequireAuth = ({ allowedRoles }) => {
 
 
     return (
-        auth?.roles?.find(role => allowedRoles?.includes(role)) ? <Outlet /> : auth?.email ? <Navigate to='/unauthorized' state={{ from: location }} replace/> : <Navigate to='/login' state={{ from: location }} replace />
+        auth?.roles?.find(role => allowedRoles?.includes(role)) ? <Outlet /> : auth?.cuil ? <Navigate to='/unauthorized' state={{ from: location }} replace/> : <Navigate to='/login' state={{ from: location }} replace />
         
     )
 }
