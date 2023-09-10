d624579803a8bd9e2570c945f12c124929ad7449
Tomas Villarreal <tomas.villarreal@unc.edu.ar>
2023-09-10 19:17:36 -0300

    Agregados estilos para no autorizado

--- src/App.js
+++ src/App.js
@@ -35,12 +35,12 @@ function App() {
   return (
       <Routes>
         <Route path='/' element={<Layout />}>
+          <Route path='unauthorized' element={<Unauthorized/>}/>
           <Route element={<PersistLoginInverse />}>
             <Route path='/' element={<Home/>}/>
             <Route path='home' element={<Home/>}/>
             <Route path='login' element={<LoginPage/>}/>
             <Route path='signup' element={<Signup/>}/>
-            <Route path='unauthorized' element={<Unauthorized/>}/>
           </Route>
           <Route element={<PersistLogin />}>
             <Route element={<RequireAuth allowedRoles={[ROLES.Admin, ROLES.ResponsableProyecto, ROLES.Evaluador, ROLES.RefEvaluador, ROLES.ComAsesora, ROLES.Docente]}/>}>
--- src/js/pages/Unauthorized.jsx
+++ src/js/pages/Unauthorized.jsx
@@ -1,9 +1,43 @@
-import React from 'react'
+// components
+import Button from '../components/Button/Button'
+import notAuthorized from '../../assets/user_not_authorized.png'
+
+// hooks
+import { useNavigate } from 'react-router-dom'
+import useLogout from '../hooks/useLogout'
 
 const Unauthorized = () => {
+  const navigate = useNavigate()
+  const logout = useLogout()
+
+  const signOut = async () => {
+      await logout()
+      navigate('/home')
+  }
+
+  const volverInicio = () => {
+    navigate('/dashboard', { replace: true })
+  }
+
   return (
     <div className='unauthorized'>
-        <h2>NO ESTAS AUTORIZADO</h2>
+        <img className='unauthorized__img' src={notAuthorized}/>
+        <h2 className='unauthorized__title'>USUARIO NO AUTORIZADO</h2>
+        <p className='unauthorized__text'>
+        No estás autorizado para entrar a esta pantalla. 
+        Si crees que esto es un error, porfavor, contáctate con cienciaconecta.utn@gmail.com
+        </p>
+        <div className='unauthorized__buttons'>
+          <Button 
+            text='Salir' 
+            onClickHandler={signOut}
+          />
+          <Button 
+            text='Inicio'
+            onClickHandler={volverInicio} 
+            activo={true}
+          />
+        </div>
     </div>
   )
 }
