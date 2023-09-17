e313f8812d6eea8b5dca0631986a9ac2df558392
Tomas Villarreal <89234039+tomzvilla@users.noreply.github.com>
2023-09-17 15:57:24 -0300

    [CNC-62] Borrar feria y borrar proyecto (#29)
    Co-authored-by: Tomas Villarreal <tomas.villarreal@unc.edu.ar>

--- src/js/components/Projects/ProjectCard.jsx
+++ src/js/components/Projects/ProjectCard.jsx
@@ -1,10 +1,97 @@
 // components
-import { Link } from "react-router-dom"
 import ProjectCardDetails from "./ProjectCardDetails"
-import Button from "../Button/Button"
+import ImageButton from "../ImageButton/ImageButton"
 import ImageLink from "../ImageLink/ImageLink"
 
+// hooks
+import { useNavigate, useLocation } from "react-router-dom"
+import useAxiosPrivate from "../../hooks/useAxiosPrivate"
+import Swal from "sweetalert2"
+
 const ProjectCard = ({ formData }) => {
+    const navigate = useNavigate()
+    const location = useLocation()
+    const from = location?.state?.from || '/dashboard'
+
+    const axiosPrivate = useAxiosPrivate()
+
+    const handleDelete = () => {
+        Swal.fire({
+            title: '¿Deseas eliminar tu proyecto?',
+            icon: 'question',
+            showCancelButton: true,
+            reverseButtons: true,
+            confirmButtonText: 'Eliminar',
+            confirmButtonColor: '#00ACE6',
+            cancelButtonText: 'Regresar',
+            cancelButtonColor: '#D4272D',
+        }).then( (result) => {
+            if(result.isConfirmed) Swal.fire({
+                title: '¿Estas seguro?',
+                text: 'La cancelación no podrá deshacerse. Ingresá el título de tu proyecto para confirmar.',
+                icon: 'warning',
+                input: 'text',
+                showCancelButton: true,
+                reverseButtons: true,
+                showLoaderOnConfirm: true,
+                confirmButtonText: 'Confirmar',
+                confirmButtonColor: '#00ACE6',
+                cancelButtonText: 'Regresar',
+                cancelButtonColor: '#D4272D',
+                preConfirm: (proyectoInput) => {
+                    try {
+                        if(proyectoInput !== formData.titulo){
+                            throw new Error('ERROR, el título de tu proyecto no coincide')
+                        }
+
+                    } catch(err) {
+                        Swal.showValidationMessage(
+                            'Error el título de tu proyecto no coincide'
+                        )
+                    }
+                },
+            }).then(async (result) => {
+                if(result.isConfirmed) {
+                    const success = await deleteProyecto()
+                    if(success) Swal.fire({
+                        title: 'Eliminaste tu proyecto',
+                        icon: 'warning',
+                        confirmButtonText: 'OK',
+                        confirmButtonColor: '#00ACE6',
+                    }).then((result) => {
+                        if(result.isConfirmed || result.isDismissed) {
+                            navigate(from, {replace: true, state: {from:`${location.pathname}`}})
+                        }
+                    })
+                }
+            })
+        })
+
+        
+    }
+    
+    const deleteProyecto = async () => {
+        try {
+            await axiosPrivate.delete(`/proyecto/${formData._id}`)
+            return true
+        } catch (err) {
+            let msg = ''
+            if(!err?.response){
+                msg = 'El servidor no respondió.'
+            } else if(err.response?.status === 401) {
+                msg = 'No estas autorizado para borrar este proyecto.'
+            } else {
+                msg = `Falló la eliminación de tu proyecto <br> ${err.response.data.error}`
+            }
+            Swal.fire({
+                html: msg,
+                title: 'Falló la eliinación',
+                icon: 'error',
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#00ACE6',
+            })
+        }
+    }
     
     return (
         <div className="project-card">
@@ -12,6 +99,7 @@ const ProjectCard = ({ formData }) => {
 
             <div className="project-card__edit">
                 <ImageLink src={require("../../../assets/edit.png")} linkto={`/editProjects/${formData._id}`} alt="Editar Proyecto"/>
+                <ImageButton small={false} alt="Borrar" linkto={""} callback={handleDelete} src={require("../../../assets/x.png")}/>
             </div>
 
             <ProjectCardDetails datos={formData}/>
--- src/js/pages/Ferias/VisualizarFeriaActual.jsx
+++ src/js/pages/Ferias/VisualizarFeriaActual.jsx
@@ -4,14 +4,102 @@ import Spinner from "../../components/Spinner/Spinner"
 // hooks
 import useAxiosFetch from "../../hooks/useAxiosFetch"
 import useAxiosPrivate from "../../hooks/useAxiosPrivate"
+import { useNavigate, useLocation } from "react-router-dom"
+import Swal from "sweetalert2"
 
 const VisualizarFeriaActual = () => {
 
     const axiosPrivate = useAxiosPrivate()
     const { data } = useAxiosFetch('/feria/activa', axiosPrivate)
+    const location = useLocation()
+    const navigate = useNavigate()
+    const from = location?.state?.from || '/dashboard'
+
+    const handleDelete = () => {
+        Swal.fire({
+            title: '¿Deseas cancelar la feria actual?',
+            icon: 'question',
+            showCancelButton: true,
+            reverseButtons: true,
+            confirmButtonText: 'Borrar',
+            confirmButtonColor: '#00ACE6',
+            cancelButtonText: 'Regresar',
+            cancelButtonColor: '#D4272D',
+        }).then( (result) => {
+            if(result.isConfirmed) Swal.fire({
+                title: '¿Estas seguro?',
+                text: 'La cancelación no podrá deshacerse. Ingresá el nombre de la feria para confirmar.',
+                icon: 'warning',
+                input: 'text',
+                showCancelButton: true,
+                reverseButtons: true,
+                showLoaderOnConfirm: true,
+                confirmButtonText: 'Confirmar',
+                confirmButtonColor: '#00ACE6',
+                cancelButtonText: 'Regresar',
+                cancelButtonColor: '#D4272D',
+                preConfirm: (feriaInput) => {
+                    try {
+                        if(feriaInput !== data?.feriaActiva?.nombre){
+                            throw new Error('ERROR, el nombre de la feria no coincide')
+                        }
+
+                    } catch(err) {
+                        Swal.showValidationMessage(
+                            'Error el nombre de la feria no coincide'
+                        )
+                    }
+                },
+            }).then(async (result) => {
+                if(result.isConfirmed) {
+                    const success = await deleteFeria()
+                    if(success) Swal.fire({
+                        title: 'Cancelaste la feria actual',
+                        icon: 'warning',
+                        confirmButtonText: 'OK',
+                        confirmButtonColor: '#00ACE6',
+                    }).then((result) => {
+                        if(result.isConfirmed || result.isDismissed) {
+                            navigate(from, {replace: true, state: {from:`${location.pathname}`}})
+                        }
+                    })
+                }
+            })
+        })
+
+        
+    }
+    
+    const deleteFeria = async () => {
+        try {
+            await axiosPrivate.delete(`/feria/${data?.feriaActiva?._id}`,
+                {
+                    withCredentials: true
+                }
+            )
+            return true
+        } catch (err) {
+            let msg = ''
+            if(!err?.response){
+                msg = 'El servidor no respondió'
+            } else if(err.response?.status === 401) {
+                msg = 'No estas autorizado para borrar la feria, tenés que ser administrador.'
+            } else {
+                msg = `Falló la cancelación de la Feria <br> ${err.response.data.error}`
+            }
+            Swal.fire({
+                html: msg,
+                title: 'Falló la cancelación',
+                icon: 'error',
+                confirmButtonText: 'OK',
+                confirmButtonColor: '#00ACE6',
+            })
+        }
+
+    }
 
     return (
-        !data ? (<Spinner />) : (<FeriaCard formData={data?.feriaActiva} />)
+        !data ? (<Spinner />) : (<FeriaCard handleDelete={handleDelete} formData={data?.feriaActiva} />)
     )
 }
 
