6381b309feaaaa100ea152a0031240f410473423
Tomas Silva Fiezzi <tomicomputer@gmail.com>
2023-10-11 23:32:41 -0300

    Blank state

--- src/js/components/BlankState/BlankState.jsx
+++ src/js/components/BlankState/BlankState.jsx
@@ -1,9 +1,10 @@
+
+
 const BlankState = (props) => {
     return (
-        <div>
-            <h2>
-                {props.msg}
-            </h2>
+        <div className="blank-state">
+            <img className="blank-state__img" src={require("../../../assets/blank.png")} alt="Blank State" />
+            <h3 className="blank-state__text"> {props.msg} </h3>
         </div>
     )
 }
