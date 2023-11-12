e3f5f7b63a5bf964cc8d17237a26147043dc03fa
Tomas Silva Fiezzi <tomicomputer@gmail.com>
2023-11-12 18:45:23 -0300

    Agregar blank states a dashboards

--- src/js/components/Dashboard/DashboardResponsable.jsx
+++ src/js/components/Dashboard/DashboardResponsable.jsx
@@ -1,46 +1,50 @@
 // components
 import ProjectCard from "../Projects/ProjectCard"
 import Spinner from "../Spinner/Spinner"
+import BlankState from "../BlankState/BlankState"
 
 // hooks
 import useAxiosPrivate from "../../hooks/useAxiosPrivate"
 import useAxiosFetch from "../../hooks/useAxiosFetch"
-
 import capitalizeEachLetter from "../../utils/utils"
+
 
 const DashboardResponsable = () => {
     const axiosPrivate = useAxiosPrivate()
 
-    const {data} = useAxiosFetch('/proyecto/misProyectos', axiosPrivate)
+    const {data, isLoading} = useAxiosFetch('/proyecto/misProyectos', axiosPrivate)
     const { data: categoriesData} = useAxiosFetch('/categoria', axiosPrivate)
     const { data: levelsData} = useAxiosFetch('/nivel', axiosPrivate)
 
-    let category = {
-        nombre: ''
-    }
-    let level = {
-        nombre: ''
-    }
+    let nombreCat = '';
+    let nombreLev = '';
+
     let proyectos = []
 
     if(data && categoriesData && levelsData) {
 
       proyectos = data.proyectos.map(obj => {
-        const category = categoriesData.categoria.find(element => element._id === obj.categoria)
-        const level = levelsData.nivel.find(element => element._id === obj.nivel)
+        nombreCat = categoriesData.categoria.find(element => element._id === obj.categoria)
+        nombreLev = levelsData.nivel.find(element => element._id === obj.nivel)
         if(obj.estado !== '6') {
-          return {...obj, categoria: category.nombre, nivel: level.nombre, nombreEscuela: capitalizeEachLetter(obj.establecimientoEducativo.nombre)}
+          return {...obj, categoria: nombreCat, nivel: nombreLev, nombreEscuela: capitalizeEachLetter(obj.establecimientoEducativo.nombre)}
         } else {
           return null
         }
       }).filter(project => project !== null)
     }
 
-
     return (
         <div>
-            {!data ? <Spinner/> : 
+            { isLoading ? <Spinner/> : 
+            
+            data ? 
             proyectos.map((proyecto, index) => <ProjectCard key={index} formData={proyecto}/>)
+
+              :
+
+            <BlankState msg="Todavía no tenés proyectos inscriptos. ¡Intentá de nuevo mas tarde!"/>
+
             }
         </div>
     )
