8d4b1ab7de4264c982af0cc69c0ea31d3abcca6f
Tomas Villarreal <89234039+tomzvilla@users.noreply.github.com>
2023-11-12 22:42:52 -0300

    Arreglado el mapeo (#76)
    Co-authored-by: Tomas Villarreal <tomas.villarreal@unc.edu.ar>

--- src/js/components/Projects/ProjectCardDetails.jsx
+++ src/js/components/Projects/ProjectCardDetails.jsx
@@ -2,19 +2,21 @@ import Badge from "../Badge/Badge"
 
 const ProjectCardDetails = (props) => {
 
-    const findNivel = (nombre) => {
-        const item = props.niveles.nivel.find((nivel) => nombre === nivel.nombre)
+    const findNivel = (nivel) => {
+        const level = nivel.nombre ?? nivel
+        const item = props.niveles.nivel.find((nivel) => level === nivel.nombre)
         return item ? item : null;
     }
 
-    const findCategoria = (nombre) => {
-        const item = props.categorias.categoria.find((categoria) => nombre === categoria.nombre)
+    const findCategoria = (categoria) => {
+        const cat = categoria.nombre ?? categoria
+        const item = props.categorias.categoria.find((categoria) => cat === categoria.nombre)
         return item ? item : null;
     }
+
 
     const level = findNivel(props.datos.nivel)
     const cat = findCategoria(props.datos.categoria)
-
 
     return (
         <div className="project-card-details">
@@ -25,7 +27,7 @@ const ProjectCardDetails = (props) => {
                 </p>
                 <p className="project-card-details__badges">
                     <strong>Categoria: </strong> 
-                    <Badge  key={cat._id} type={cat}/>
+                    <Badge  key={cat?._id} type={cat}/>
                 </p>
                 <p className="project-card-details__detail">
                     <strong>Escuela: </strong> 
@@ -33,7 +35,7 @@ const ProjectCardDetails = (props) => {
                 </p>
                 <p className="project-card-details__badges">
                     <strong>Nivel: </strong> 
-                    <Badge  key={level._id} type={level}/>
+                    <Badge  key={level?._id} type={level}/>
                 </p>
                 <p className="project-card-details__detail">
                     <strong>Estado: </strong> 
