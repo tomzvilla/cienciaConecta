1c007f1c7331e3c4daeacd409718cf1fdd7488bd
Tomas Villarreal <89234039+tomzvilla@users.noreply.github.com>
2023-11-29 22:12:38 -0300

    [CNC-179] Agregar consulta para saber si el usuario puede postularse (#100)
    Co-authored-by: Tomas Villarreal <tomas.villarreal@unc.edu.ar>

--- src/js/pages/Evaluadores/Postulacion.jsx
+++ src/js/pages/Evaluadores/Postulacion.jsx
@@ -3,10 +3,13 @@ import PostulacionDocenteForm from "../../components/Postulacion/PostulacionDoce
 import Metadata from "../../components/Metadata/Metadata"
 import BlankState from "../../components/BlankState/BlankState"
 import DocenteEvaluadorCard from "./DocenteEvaluadorCard"
+import Spinner from "../../components/Spinner/Spinner"
 import Card from "../../components/Card/Card"
 // hooks
 import { useState } from "react"
 import { useSelector } from "react-redux"
+import useAxiosFetch from "../../hooks/useAxiosFetch"
+import useAxiosPrivate from "../../hooks/useAxiosPrivate"
 import useAuth from "../../hooks/useAuth"
 import useUtils from "../../hooks/useUtils"
 import { ROLES } from "../../../App"
@@ -18,14 +21,25 @@ const Postulacion = () => {
     const fecha = new Date()
     const { auth } = useAuth()
 
+    const axiosPrivate = useAxiosPrivate()
+
+    const { isLoading, status, error } = useAxiosFetch('/evaluador/validarPostulacion',axiosPrivate)
+
     return (
         <>
             <Metadata title={'Postularme como Evaluador'}/>
-            { !feria ?
+            { 
+            isLoading ?
+            <Spinner />
+            :
+            !feria ?
             <Card title={'Postulación'}><BlankState msg={`No hay ninguna Feria de Ciencias y Tecnología activa en este momento. Podes acceder a nuestros canales oficiales para tener más detalles del calendario.`}/></Card>
             :
             auth?.roles?.find(role => role === ROLES.RefEvaluador ) ?
             <Card title={'Postulación'}><BlankState msg={`Un referente de evaluador no puede postularse como evaluador.`}/></Card>
+            :
+            status > 399 ?
+            <Card title={'Postulación'}><BlankState msg={`No puedes postularte como evaluador \n ${error.response.data.error}`}/></Card>
             :
             fecha >= new Date(feria?.fechas_evaluador.fechaInicioPostulacionEvaluadores) && fecha < new Date(feria?.fechas_evaluador.fechaFinPostulacionEvaluadores) ?
             <>
