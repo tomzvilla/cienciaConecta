ceaafbd3f459632c8e13b8d44a2a29ed3b388848
Tomas Silva Fiezzi <79322879+tsilva9@users.noreply.github.com>
2023-12-05 12:53:20 -0300

    Arreglos cargar establecimiento (#105)
    * Arreglos varios de estilos
    
    * Arreglos cargar establecimiento

--- src/js/components/CargarEstablecimientos/CargarEstablecimientos.jsx
+++ src/js/components/CargarEstablecimientos/CargarEstablecimientos.jsx
@@ -1,9 +1,10 @@
 import Button from "../Button/Button";
 import FileField from "../FileField/FileField";
 import dropdown from '../../../assets/down-arrow.png'
+import { useState } from "react";
 
 const CargarEstablecimientos = (props) => {
-
+    const [showDropdown, setShowDropdown] = useState(false)
     const {formValues, setFormValues, validateForm, errors, onBlurField, handleSubmit} = props
 
     const handleFileChange = (e) => {
@@ -19,15 +20,24 @@ const CargarEstablecimientos = (props) => {
         }
     }
 
+    const handleDropdown = () => {
+        let value = !showDropdown
+
+        setShowDropdown(value)
+    }
+
     return (
             <div className="cargar-establecimientos">
-                <div>
-                    <h4>Criterios a seguir en el Excel</h4>
+                <p className="cargar-establecimientos__text">Para cargar los establecimientos, por favor carga el Excel y tené en cuenta los criterios del archivo:</p>
+                <div className="cargar-establecimientos__dropdown-title" onClick={handleDropdown}>
+                    <p>Criterios a seguir en el Excel</p>
                     <img src={dropdown} alt="dropdown arrow" />
                 </div>
-                <div>
-                    <p>El padrón con establecimientos educativos se debe obtener de la página oficial de la Nacion, desde  
-                        <a href="https://www.argentina.gob.ar/educacion/evaluacion-e-informacion-educativa/padron-oficial-de-establecimientos-educativos" target="_blank" rel="noopener noreferrer"> aquí</a>.
+
+                {showDropdown ? 
+                <div className="cargar-establecimientos__dropdown-text">
+                    <p>El padrón con establecimientos educativos se debe obtener de la página oficial de la Nación, desde  {" "}
+                        <a className="cargar-establecimientos__link" href="https://www.argentina.gob.ar/educacion/evaluacion-e-informacion-educativa/padron-oficial-de-establecimientos-educativos" target="_blank" rel="noopener noreferrer">esta página</a>.
                         El archivo debe contener un mínimo de 5000 filas (establecimientos) y debe tener una sola hoja con las siguientes columnas (orden y nombres):
                     </p>
                     <ul>
@@ -42,16 +52,20 @@ const CargarEstablecimientos = (props) => {
                         <li>Columna M: Email</li>
                     </ul>
                 </div>
-                <p className="cargar-establecimientos__text">Para cargar los establecimientos, por favor carga el excel utilizando el botón debajo:</p>
-                <FileField
-                label='Establecimientos: ' 
-                name='excel'
-                onChange={handleFileChange}
-                onBlur={onBlurField}
-                errors={errors.excel}
-                accept={'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}
-                required={true}
-                />
+                    : ""
+                }
+                
+                <div className="cargar-establecimientos__file-field">
+                    <FileField
+                    label='Establecimientos: ' 
+                    name='excel'
+                    onChange={handleFileChange}
+                    onBlur={onBlurField}
+                    errors={errors.excel}
+                    accept={'application/vnd.openxmlformats-officedocument.spreadsheetml.sheet'}
+                    required={true}
+                    />
+                </div>
                 <div className="cargar-establecimientos__button">
                     <Button text="Cargar" activo={true} onClickHandler={handleSubmit}/>
                 </div>
