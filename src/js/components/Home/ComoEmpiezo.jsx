c8fd11a3db971302f9e368fccd3a0567612e4e3b
Tomas Villarreal <89234039+tomzvilla@users.noreply.github.com>
2023-12-14 20:16:25 -0300

    Cambio imagenes del home de jpg a webp (#123)
    Co-authored-by: Tomas Villarreal <tomas.villarreal@unc.edu.ar>

--- src/js/components/Home/ComoEmpiezo.jsx
+++ src/js/components/Home/ComoEmpiezo.jsx
@@ -4,7 +4,7 @@ import Button from "../Button/Button";
 const ComoEmpiezo = (props) => {
     return (
         <div className="como-empiezo">
-            {window.innerWidth >= 800 ? <img className="como-empiezo__img" src={require("../../../assets/empiezo.jpg")} alt="" /> : ""}
+            {window.innerWidth >= 800 ? <img className="como-empiezo__img" src={require("../../../assets/empiezo.webp")} alt=""  /> : ""}
             
             <h4 className="como-empiezo__title">¿Cómo empiezo?</h4>
             <p className="como-empiezo__text">Para ingresar a Ciencia Conecta necesitas una cuenta, con la que vas a poder postularte como evaluador e inscribir y gestionar proyectos</p>
@@ -12,7 +12,7 @@ const ComoEmpiezo = (props) => {
                 <Button text="Ingresá" onClickHandler={props.openModal}/>
             </div>
 
-            {window.innerWidth <= 800 ? <img className="como-empiezo__img" src={require("../../../assets/hero.jpg")} alt="" /> : ""}
+            {window.innerWidth <= 800 ? <img className="como-empiezo__img" src={require("../../../assets/hero.webp")} alt="" /> : ""}
             
         </div>
     );
